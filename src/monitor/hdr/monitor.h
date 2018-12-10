09462c6730ba19c5c3a2a3313a17859c6a8e5ed0
GEUMKANG <a01039456218@gmail.com>
2018-12-10 20:17:08 +0900

    oled

--- src/monitor/hdr/monitor.h
+++ src/monitor/hdr/monitor.h
@@ -3,7 +3,7 @@
 
 #include <sensor_data.h>
 #include <time.h>
-#include "SSD1306.h"
+//#include "SSD1306.h"
 char value[10]={'0','1','2','3','4','5','6','7','8','9'};
 
 void monitor(sensor_data **data, int len);
