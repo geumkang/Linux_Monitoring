1d07392ec4881c8d2246996ad5f404bf7fec8ba8
GEUMKANG <a01039456218@gmail.com>
2018-12-10 20:41:39 +0900

    oled

--- src/monitor/src/monitor.c
+++ src/monitor/src/monitor.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <time.h>
 #include "SSD1306.h"
+#include <bcm2835.h>
+
 char value[10]={'0','1','2','3','4','5','6','7','8','9'};
 
 void monitor(sensor_data **data, int len){
