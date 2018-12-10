4610f506900fa492f279b1facb112303c9a6bd54
GEUMKANG <a01039456218@gmail.com>
2018-12-10 20:06:57 +0900

    oled

--- src/main/src/main.c
+++ src/main/src/main.c
@@ -19,8 +19,8 @@ int main(){
 	int nData = 3;
 	sensor_data **data = NULL;
 	data = (sensor_data *)malloc(sizeof(sensor_data*) * nData);
-	printf("sdadakakakakk");
-	oled_init();
+	fprintf(stdout, "sdadakakakakk");
+	//oled_init();
 	for(i = 0 ; i < nData ; i++) {
 		data[i] = sensor_data_init();
 	}
