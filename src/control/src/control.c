84296778c8709a32d828b27af2b6283677b9d579
GEUMKANG <a01039456218@gmail.com>
2018-12-11 14:35:37 +0900

    key

--- src/control/src/control.c
+++ src/control/src/control.c
@@ -18,9 +18,12 @@ int key_run(){
 	if(bcm2835_gpio_lev(PRESSKEY) == 0)
 	{  
 		printf ("KEY PRESS\n") ;
+		while(bcm2835_gpio_lev(PRESSKEY) == 0)
+			bcm2835_delay(100);
 		return 1;
 	}
-	else return 0;
+	bcm2835_delay(100);
+	return 0;
 }
 
 void key_end(){
