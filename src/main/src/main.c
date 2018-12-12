a13cb6b1d48db29c98cbbed4ad821cea4b11e0c6
GEUMKANG <a01039456218@gmail.com>
2018-12-12 22:49:48 +0900

    Update main.c

--- src/main/src/main.c
+++ src/main/src/main.c
@@ -61,7 +61,7 @@ int main(){
 		monitor(data, nData);
 		char* currentValue = malloc(40 * sizeof(char));
 		
-		if(*view != 0 && (menu == UP || menu == DOWN)){
+		if(*view != 0){
 			sprintf(currentValue, "%s", data[currentUnit]->unit);
 		}
 		else{
