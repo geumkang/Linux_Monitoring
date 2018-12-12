c974e60654d7121dda3a59d73b38f511dc2dd733
GEUMKANG <a01039456218@gmail.com>
2018-12-12 22:18:20 +0900

    Update monitor.c

--- src/monitor/src/monitor.c
+++ src/monitor/src/monitor.c
@@ -59,10 +59,14 @@ void oled_run(char* temp, char* press, char* alti, char* selectedValue, int menu
 		}
 		if(menu == UP) minValue++;
 		else if(menu == DOWN) minValue--;
+		printf("2132\n");
 		char* Description = malloc(40 * sizeof(char));
 		sprintf(Description, "%s", "Min Value");
+		printf("2132\n");
 		SSD1306_string(0, 16, Description, 12, 1);
+		printf("2132\n");
 		displayValue(minValue);
+		printf("2132\n");
 	}
 	
 	SSD1306_display();
