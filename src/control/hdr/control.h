42bd84c0730b6adcab0f914fd10ef0a8681a6460
GEUMKANG <a01039456218@gmail.com>
2018-12-12 18:34:24 +0900

    key

--- src/control/hdr/control.h
+++ src/control/hdr/control.h
@@ -4,4 +4,8 @@
 int key_init();
 int key_run();
 void key_end();
+
+void i2c_writeByte(char byte);
+char i2c_readByte();
+
 #endif
--- src/control/src/control.c
+++ src/control/src/control.c
@@ -9,22 +9,60 @@ int key_init(){
 	if (!bcm2835_init())return 1;
 	bcm2835_gpio_fsel(PRESSKEY, BCM2835_GPIO_FSEL_INPT);
 	bcm2835_gpio_set_pud(PRESSKEY, BCM2835_GPIO_PUD_UP);
+	bcm2835_i2c_setSlaveAddress(0x20);  
+    bcm2835_i2c_set_baudrate(10000);  
 	printf("Key Test Program!!!!\n");	
 	
 }
 
 int key_run(){
-	
+	char value;
+	i2c_writeByte(0x0F | i2c_readByte());
+	value = i2c_readByte() | 0xF0;
 	if(bcm2835_gpio_lev(PRESSKEY) == 0)
 	{  
 		printf ("KEY PRESS\n") ;
-		// while(bcm2835_gpio_lev(PRESSKEY) == 0)
-		// 	bcm2835_delay(100);
+		while(bcm2835_gpio_lev(PRESSKEY) == 0)
+			bcm2835_delay(10);
 		return 1;
 	}
+	else if(value != 0xFF)
+		{
+			switch(value)
+			{	
+				case 0xFE:
+					printf("left\n");break;	
+				case 0xFD:
+					printf("up\n");	break;
+				case 0xFB:
+					printf("dowm\n");break;	
+				case 0xF7:
+					printf("right\n");break;
+				default :
+					printf("unknow\n");
+			}
+			while(value != 0xFF)
+			{
+				i2c_writeByte(0x0F | i2c_readByte());
+				value = i2c_readByte() | 0xF0;
+				bcm2835_delay(10);
+			}
+		}
 	return 0;
 }
 
 void key_end(){
 	bcm2835_close();
+}
+
+void i2c_writeByte(char byte)
+{
+	char buf[] = {byte};
+	bcm2835_i2c_write(buf,1);
+}
+char i2c_readByte()
+{
+	char buf[1];
+	bcm2835_i2c_read(buf,1);
+	return buf[0];
 }
