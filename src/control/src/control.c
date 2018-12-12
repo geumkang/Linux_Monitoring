30abd0f66141efd687a9d235da14023f56f94d9d
GEUMKANG <a01039456218@gmail.com>
2018-12-12 20:50:01 +0900

    Update control.c

--- src/control/src/control.c
+++ src/control/src/control.c
@@ -60,14 +60,12 @@ int key_run(){
 				default :
 					printf("unknow\n");
 			}
-			// while(value != 0xFF)
-			// {
-			// 	i2c_writeByte(0x0F | i2c_readByte());
-			// 	value = i2c_readByte() | 0xF0;
-			// 	bcm2835_delay(10);
-			// }
-			
-			//bcm2835_i2c_end();  
+			while(value != 0xFF)
+			{
+				i2c_writeByte(0x0F | i2c_readByte());
+				value = i2c_readByte() | 0xF0;
+				bcm2835_delay(10);
+			}
 		}
 		bcm2835_i2c_setSlaveAddress(0x77);  
     	bcm2835_i2c_set_baudrate(10000);  		
